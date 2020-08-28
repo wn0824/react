1a4f9aefd0a5bb001ee5164090ed46becba941b6
oojMAC <wn0824@gmail.com>
2020-08-28 09:56:55 +0900

    today

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,12 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
 // import Counter from './Counter';
-import IterationSample from './IterationSample';
+import LifeCycleSample from './LifeCycleSample';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random()*16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
   render() {
     return (
-      <IterationSample />
+      <div>
+        <button onClick={this.handleClick}>random</button>
+        <LifeCycleSample color={this.state.color} />
+      </div>
     )
   }
 }
